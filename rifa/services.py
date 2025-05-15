ae3609d1cc796e4c729b827faaf3ed4ac2334320
Jhos10 <jhostinposada7@gmail.com>
2025-05-15 11:58:49 -0500

    Se borraron las funciones que estaban comentadas para tener un codigo mas limpio

--- rifa/services.py
+++ rifa/services.py
@@ -13,14 +13,6 @@ def obtener_rifas(session: session_dependency):
         raise ValueError("No se encontraron rifas")
     return rifas
 
-
-# def obtener_rifa(session: session_dependency, id: int):
-#     rifa = session.get(Rifa, id)
-#     if not rifa:
-#         raise ValueError(f"No se encontro la rifa con id{id}")
-#     return rifa
-
-
 def crear_rifa(session: session_dependency, rifa: models.RifaCreate):
     try:
         nueva_rifa = Rifa(**rifa.model_dump())
@@ -31,37 +23,6 @@ def crear_rifa(session: session_dependency, rifa: models.RifaCreate):
     except Exception as e:
         raise RifaCreationError(error=str(e))
 
-# def agregar_numero_especial(session: session_dependency, numero_especial: models.Numero_especial):
-#     id_rifa = numero_especial.id_rifa
-#     rifa = session.get(Rifa, id_rifa)
-#     # Validamos que exista la rifa y este activa
-#     if not rifa or rifa.is_active == False:
-#         raise ValueError(f"No se pudo agregar el numero especial a la rifa con id : {id_rifa}")
-#     # Consultamos los numeros especiales que pertenecen a la rifa    
-#     numeros_especiales = session.exec(select(entities.Numero).where(entities.Numero.id_rifa == id_rifa)).all()
-#     # Validamos que la rifa no tenga un numero especial con el mismo numero
-#     for numero in numeros_especiales:
-#         if numero.numero == numero_especial.numero:
-#             raise ValueError(f"Ya existe un numero especial con el numero {numero_especial.numero}")
-#     session.add(numero_especial)
-#     session.commit()
-#     session.refresh(numero_especial)
-#     return numero_especial 
- 
-# def obtener_ganador(session: session_dependency, boleta: models.BoletaConsulta):
-#     boleta_obtenida = session.get(entities.Boleta, boleta.id)
-#     if not boleta_obtenida:
-#         raise ValueError(
-#             f"No se encontro la boleta con el numero {boleta.numero} y rifa {boleta.id_rifa}"
-#         )
-#     usuario = session.exec(
-#         select(entities.User).where(entities.User.id == boleta_obtenida.id_usuario)).first()
-#     if not usuario:
-#         raise ValueError(
-#             f"No se encontro el usuario con el id {boleta_obtenida.id_usuario}"
-#         )
-#     return usuario
-
 def finalizar_rifa(session: session_dependency, id: int):
     rifa = session.get(Rifa.Rifa, id)
     if not rifa and rifa.is_active == False:
@@ -71,32 +32,6 @@ def finalizar_rifa(session: session_dependency, id: int):
     session.commit()
     session.refresh(rifa)
     return rifa
-
-
-# def agregar_numero_especial(
-#     session: session_dependency, numero_especial: models.Numero_especial
-# ):
-#     id_rifa = numero_especial.id_rifa
-#     rifa = session.get(Rifa.Rifa, id_rifa)
-#     # Validamos que exista la rifa y este activa
-#     if not rifa or rifa.is_active == False:
-#         raise ValueError(
-#             f"No se pudo agregar el numero especial a la rifa con id : {id_rifa}"
-#         )
-#     # Consultamos los numeros especiales que pertenecen a la rifa
-#     numeros_especiales = session.exec(
-#         select(entities.Numero).where(entities.Numero.id_rifa == id_rifa)
-#     ).all()
-#     # Validamos que la rifa no tenga un numero especial con el mismo numero
-#     for numero in numeros_especiales:
-#         if numero.numero == numero_especial.numero:
-#             raise ValueError(
-#                 f"Ya existe un numero especial con el numero {numero_especial.numero}"
-#             )
-#     session.add(numero_especial)
-#     session.commit()
-#     session.refresh(numero_especial)
-#     return numero_especial
 
 
 # def obtener_ganador(session: session_dependency, boleta: models.BoletaConsulta):
