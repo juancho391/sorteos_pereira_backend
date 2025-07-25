d8bd77de52277c1c8383c85518c291bb5ee7fc69
juancho391 <juanchischis2010@hotmail.com>
2025-07-25 11:40:03 -0500

    elimine comentario innecesario

--- numeros/controller.py
+++ numeros/controller.py
@@ -13,10 +13,3 @@ def crear_numero_especial(
     numero_service: numero_service_dependency,
 ):
     return numero_service.crear_numero_especial(numero_especial=numero_especial)
-
-
-# @router_numeros.delete("/{numero}/{id_rifa}")
-# def eliminar_numero_especial(numero: int, id_rifa: int, sesesion: session_dependency):
-#     return numeroService.eliminar_numero_especial(
-#         numero=numero, session=sesesion, id_rifa=id_rifa
-#     )
